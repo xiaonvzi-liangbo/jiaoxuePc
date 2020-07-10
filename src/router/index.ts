b6c57f4fa7d887c499625f7d863ac6e23aeaa6a7
tfs-user4 <642436011@qq.com>
2020-07-10 11:21:10 +0800

    删除多于页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,42 +4,8 @@ import Router, { RouteConfig } from 'vue-router'
 /* Layout */
 import Layout from '@/layout/index.vue'
 
-/* Router modules */
-import componentsRouter from './modules/components'
-import chartsRouter from './modules/charts'
-import tableRouter from './modules/table'
-import nestedRouter from './modules/nested'
 
 Vue.use(Router)
-
-/*
-  Note: sub-menu only appear when children.length>=1
-  Detail see: https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
-*/
-
-/*
-  name:'router-name'             the name field is required when using <keep-alive>, it should also match its component's name property
-                                 detail see : https://vuejs.org/v2/guide/components-dynamic-async.html#keep-alive-with-Dynamic-Components
-  redirect:                      if set to 'noredirect', no redirect action will be trigger when clicking the breadcrumb
-  meta: {
-    roles: ['admin', 'editor']   will control the page roles (allow setting multiple roles)
-    title: 'title'               the name showed in subMenu and breadcrumb (recommend set)
-    icon: 'svg-name'             the icon showed in the sidebar
-    hidden: true                 if true, this route will not show in the sidebar (default is false)
-    alwaysShow: true             if true, will always show the root menu (default is false)
-                                 if false, hide the root menu when has less or equal than one children route
-    breadcrumb: false            if false, the item will be hidden in breadcrumb (default is true)
-    noCache: true                if true, the page will not be cached (default is false)
-    affix: true                  if true, the tag will affix in the tags-view
-    activeMenu: '/example/list'  if set path, the sidebar will highlight the path you set
-  }
-*/
-
-/**
-  ConstantRoutes
-  a base page that does not have permission requirements
-  all roles can be accessed
-*/
 export const constantRoutes: RouteConfig[] = [
   {
     path: '/redirect',
@@ -95,69 +61,18 @@ export const constantRoutes: RouteConfig[] = [
       }
     ]
   },
- 
-  {
-    path: '/guide',
-    component: Layout,
-    redirect: '/guide/index',
-    children: [
-      {
-        path: 'index',
-        component: () => import(/* webpackChunkName: "guide" */ '@/views/guide/index.vue'),
-        name: 'Guide',
-        meta: {
-          title: 'guide',
-          icon: 'guide',
-          noCache: true
-        }
-      }
-    ]
-  },
-  {
-    path: '/profile',
-    component: Layout,
-    redirect: '/profile/index',
-    meta: { hidden: true },
-    children: [
-      {
-        path: 'index',
-        component: () => import(/* webpackChunkName: "profile" */ '@/views/profile/index.vue'),
-        name: 'Profile',
-        meta: {
-          title: 'profile',
-          icon: 'user',
-          noCache: true
-        }
-      }
-    ]
-  }
+
+
 ]
 export const asyncRoutes: RouteConfig[] = [
-  
-  /** when your routing map is too long, you can split it into small modules **/
- 
-  {
-    path: '/tab',
-    component: Layout,
-    children: [
-      {
-        path: 'index',
-        component: () => import(/* webpackChunkName: "tab" */ '@/views/tab/index.vue'),
-        name: 'Tab',
-        meta: {
-          title: 'tab',
-          icon: 'tab'
-        }
-      }
-    ]
-  },
   {
     path: '/error',
     component: Layout,
     redirect: 'noredirect',
     meta: {
       title: 'errorPages',
-      icon: '404'
+      icon: '404',
+      hidden: true 
     },
     children: [
       {
@@ -179,60 +94,7 @@ export const asyncRoutes: RouteConfig[] = [
         }
       }
     ]
-  },
-  {
-    path: '/error-log',
-    component: Layout,
-    redirect: 'noredirect',
-    children: [
-      {
-        path: 'log',
-        component: () => import(/* webpackChunkName: "error-log" */ '@/views/error-log/index.vue'),
-        name: 'ErrorLog',
-        meta: {
-          title: 'errorLog',
-          icon: 'bug'
-        }
-      }
-    ]
-  },
-  {
-    path: '/pdf-download-example',
-    component: () => import(/* webpackChunkName: "pdf-download-example" */ '@/views/pdf/download.vue'),
-    meta: { hidden: true }
   }, 
-  {
-    path: '/clipboard',
-    component: Layout,
-    redirect: 'noredirect',
-    children: [
-      {
-        path: 'index',
-        component: () => import(/* webpackChunkName: "clipboard" */ '@/views/clipboard/index.vue'),
-        name: 'Clipboard',
-        meta: {
-          title: 'clipboard',
-          icon: 'clipboard'
-        }
-      }
-    ]
-  },
-  {
-    path: '/i18n',
-    component: Layout,
-    children: [
-      {
-        path: 'index',
-        component: () => import(/* webpackChunkName: "i18n-demo" */ '@/views/i18n-demo/index.vue'),
-        name: 'I18n',
-        meta: {
-          title: 'i18n',
-          icon: 'international'
-        }
-      }
-    ]
-  },
- 
   {
     path: '*',
     redirect: '/404',
